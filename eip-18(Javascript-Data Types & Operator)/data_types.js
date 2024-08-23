16bfb78a214541da19047a3d8a6e52335d41d8b3
RAZU-sheikh <razu351724@gmail.com>
2024-08-24 00:05:12 +0600

    javaScript Operators use

---
+++ eip-18(Javascript-Data Types & Operator)/data_types.js
@@ -0,0 +1,29 @@
+let cuntrName = 'Bangladesh'; // string
+let capital = 'Dhaka';
+let sentence = `${capital} is the capital of ${cuntrName}`;//template literal
+
+console.log(sentence)
+
+console.log(typeof cuntrName); // to findout the type of a varible
+
+//Numbers
+let age = 20;
+let height = 5.5;
+console.log(typeof age);
+
+// Boolean
+let isHot = true;
+let isCold = false;
+console.log(typeof isCold);// boolean
+
+//undefined
+let car;
+console.log(typeof car);  //undefined
+
+//Null
+let house = null; // null means empty/ on value
+console.log(typeof house); // নাল এর ডাটা টাইপ কি হয় উওর object
+
+//NaN - not a number
+let counter = 35;
+console.log(typeof counter * 'word'); //nan
---
+++ eip-18(Javascript-Data Types & Operator)/operators.js
@@ -0,0 +1,35 @@
+// Exponentiation-এক্সপোনেনসন
+// console.log(3 ** 2); //3 x 3 =9
+// console.log(2 ** 3); // 2 x 2 x 2 = 8
+// console.log(10 ** 3); // 10 x 10 x 10 = 100
+
+// % - Modulus Operators
+console.log(50 % 9);
+
+// ++ Increment
+let add = 10;
+add++;
+add++;
+console.log(add) //12
+
+// -- Decrement
+ add--;
+ add--;
+ add--;
+ console.log(add);// 9
+
+// ==
+let marks = 100;
+console.log(marks == 200); // false
+
+let  operators = '100';
+console.log(operators == 100); // true - equal
+console.log(operators != 100); // false - not equal
+
+// ===
+let play = '300';
+console.log(play === 300) // false - equal
+console.log(play !== 300) // true
+
+// == equal টাইপ চেক করে না।।
+// === equal টাইপ এবং ভেলু দুটই চেক করে।। === strict equality

ca51dbc166019c7007e5ebb7065b2d1ad200a75b
RAZU-sheikh <razu351724@gmail.com>
2024-09-14 22:17:14 +0600

    epi 21 logical  operators, bojects

---
+++ epi-21(Logical Operators, Objects, Functions)/app.js
@@ -0,0 +1,21 @@
+let userName = 'razu351724@gmail.com';
+let password = 'razu123';
+let pincode = '1234';
+
+if (userName == 'razu351724@gmail.com' || password == 'razu123' || pincode == 234){console.log('user is loged in!')}else{
+    console.log("wrong credentials");
+}
+// ||= কোনো একটা ভেলু মিলে গেলে টুরু
+// && = সব ভেলু যদি না মিললে ফলস
+
+let person = {
+    name: 'moriam',
+    age:25,
+    isMarried: true
+}
+
+// Using Dot Notation:
+ console.log(person.name);
+
+ // Using Bracket Notation:
+ console.log(person["isGraduated"]);
